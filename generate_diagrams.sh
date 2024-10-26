78c172f2b265964d0d92c7f5943fdf0f4c43eead
ChuzaWick420 <zaiddadev@gmail.com>
2024-10-27 02:29:35 +0500

    Testing: updated script - attempt 6

--- generate_diagrams.sh
+++ generate_diagrams.sh
@@ -3,13 +3,8 @@
 # Define the parent directory where the search will start
 repo_root=$(pwd)
 
-# Debugging runner dir layout
-pwd
-
-parentDirectory="$repo_root/notes_publisher/docs/"
-
 # Get all subdirectories named 'figures' within the parent directory, pipe the list into while loop
-find "$parentDirectory" -type d -name "figures" | while IFS= read -r dir; do 
+find "$repo_root" -type d -name "figures" | while IFS= read -r dir; do 
 
 # Iterate over each 'figures' directory
     find "$dir" -maxdepth 1 -name "*.py" | python
