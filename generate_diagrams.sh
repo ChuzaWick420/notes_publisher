69c1f3bb0f63435ebc5ec138dad8ba2898d64a68
ChuzaWick420 <zaiddadev@gmail.com>
2024-10-27 02:16:00 +0500

    Testing: updated script

--- generate_diagrams.sh
+++ generate_diagrams.sh
@@ -4,24 +4,11 @@
 repo_root=$(pwd)
 parentDirectory="./docs/"
 
-# Get all subdirectories named 'figures' within the parent directory
-figuresDirs=$(find "$parentDirectory" -type d -name "figures")
+# Get all subdirectories named 'figures' within the parent directory, pipe the list into while loop
+find "$parentDirectory" -type d -name "figures" | while IFS= read -r dir; do 
 
 # Iterate over each 'figures' directory
-for dir in $figuresDirs; do
-    # Find all Python scripts (.py files) within the current 'figures' directory
-    pythonScripts=$(find "$dir" -maxdepth 1 -name "*.py")
-
-    cd "$dir"
-
-    echo "$dir"
-
-    for script in $pythonScripts; do
-
-        # Run each Python script using python command
-        echo "Running script: $script"
-        python "$script"
-        
+    find "$dir" -maxdepth 1 -name "*.py" | python
         # Optionally, check if the Python script ran successfully
         if [[ $? -ne 0 ]]; then
             echo "Failed to run script: $script" >&2
@@ -33,7 +20,6 @@ for dir in $figuresDirs; do
 
     cd "$repo_root"
     cd ../
-done
 
 echo "All scripts executed."
 
