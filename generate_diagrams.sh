db84af627e2d0894b96c141fb230180d76f451a7
ChuzaWick420 <zaiddadev@gmail.com>
2024-10-30 04:23:29 +0500

    Updated: generate_diagrams.sh

--- generate_diagrams.sh
+++ generate_diagrams.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
 
 repo_root=$(pwd)
+
+# directory to search for
 parentDirectory="docs"
 
-find "$parentDirectory" -type d -name "figures" | while IFS= read -r dir; do
+# Find full path for figures
+find "$parentDirectory" -type d -name "Figures" | while IFS= read -r dir; do
 
+    # Track where we are currently as a snapshot for reference
     current_dir=$(pwd)
 
+    # change into the directory for Figures
     cd "$dir"
 
 
+    # Run each python script
     ls | while IFS= read -r script; do
         python $script
     done
 
+    # reset back to the snapshot
     cd "$current_dir"
 
 done
-
-cd "$repo_root"
 
 echo "All scripts executed."
